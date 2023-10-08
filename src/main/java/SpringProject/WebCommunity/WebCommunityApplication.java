5e566cb048a814c94654fabe39d28e5b22f53f9e
Park Seung Ho <dreamhigh924@gmail.com>
2023-10-08 17:13:51 +0900

    Spring Project generation with Gradle
    Gradle 빌드 툴로 Spring 초기 환경 생성

---
+++ src/main/java/SpringProject/WebCommunity/WebCommunityApplication.java
@@ -0,0 +1,13 @@
+package SpringProject.WebCommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SpringProject/WebCommunity/WebCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringProject.WebCommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
