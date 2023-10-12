69329c2c180ebb34d19f8aebe0b5960a884316b4
Park Seung Ho <dreamhigh924@gmail.com>
2023-10-12 21:48:07 +0900

    Moving Merged Files

--- src/main/java/SpringProject/WebCommunity/WebCommunityApplication.java
+++ src/main/java/SpringProject/WebCommunity/WebCommunityApplication.java
@@ -2,8 +2,9 @@ package SpringProject.WebCommunity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import jakarta.persistence.Entity;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //JPA Auditing 활성화
 @SpringBootApplication
 public class WebCommunityApplication {
 
--- src/test/java/SpringProject/WebCommunity/WebCommunityApplicationTests.java
+++ src/test/java/SpringProject/WebCommunity/WebCommunityApplicationTests.java
@@ -2,7 +2,6 @@ package SpringProject.WebCommunity;
 
 import SpringProject.WebCommunity.Domain.Post;
 import SpringProject.WebCommunity.Repository.PostRepos;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
