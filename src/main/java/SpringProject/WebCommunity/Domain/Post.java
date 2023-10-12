0401ad231da7a6c4bc623e5560830057ff3a3e1f
Park Seung Ho <dreamhigh924@gmail.com>
2023-10-12 15:08:47 +0900

    Test JPA

--- src/main/java/SpringProject/WebCommunity/Domain/Post.java
+++ src/main/java/SpringProject/WebCommunity/Domain/Post.java
@@ -12,13 +12,18 @@ public class Post {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // PK auto_increment 옵션
     private Long id; //id 필드
-    @Column
+    @Column(length = 64,  unique = true)
     private String writerId;
-    @Column int views;
-    @Column int likes;
-    @Column(length = 400, nullable = false) String title;
-    @Column(length = 2000, nullable = false) String contents;
-    @Column Timestamp createTime;
+    @Column(columnDefinition = "INT default 0")
+    int views;
+    @Column(columnDefinition = "INT default 0")
+    int likes;
+    @Column(length = 400, nullable = false)
+    String title;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    String contents;
+    @Column(columnDefinition = "INT default 0")
+    Timestamp createTime;
 
     @Builder // setter 역할
     public Post(String title, String contents, int views, int likes) {
