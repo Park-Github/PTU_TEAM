eee48920f5e378f490d1d869e7948a18c2829cf1
LazoYoung <idealproduction@naver.com>
2023-10-29 21:16:37 +0900

    REST API 응답 서비스 구현

---
+++ src/main/java/SpringProject/WebCommunity/Service/ResponseService.java
@@ -0,0 +1,49 @@
+package SpringProject.WebCommunity.Service;
+
+import SpringProject.WebCommunity.Model.Response.ListResponse;
+import SpringProject.WebCommunity.Model.Response.Response;
+import SpringProject.WebCommunity.Model.Response.SingleResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ResponseService {
+
+    public Response getResponse(boolean success) {
+        Response response = new Response();
+        if (success) {
+            markSuccess(response);
+        } else {
+            markFail(response);
+        }
+        return response;
+    }
+
+    public <T> SingleResponse<T> getSingleResponse(T data) {
+        SingleResponse<T> response = new SingleResponse<>();
+        response.setData(data);
+        markSuccess(response);
+        return response;
+    }
+
+    public <T> ListResponse<T> getListResponse(List<T> list) {
+        ListResponse<T> response = new ListResponse<>();
+        response.setList(list);
+        markSuccess(response);
+        return response;
+    }
+
+    private <T extends Response> void markSuccess(T response) {
+        response.setSuccess(true);
+        response.setStatus(0);
+        response.setMessage("Success.");
+    }
+
+    private <T extends Response> void markFail(T response) {
+        response.setSuccess(false);
+        response.setStatus(1);
+        response.setMessage("Fail.");
+    }
+
+}
