1bbea9ce8e9926755500355d5a877e9b47aff15d
Park Seung Ho <dreamhigh924@gmail.com>
2023-10-29 13:41:01 +0900

    market 페이지 thymeleaf 오류 수정 member entity 필드 수정

--- src/main/java/SpringProject/WebCommunity/Domain/Member.java
+++ src/main/java/SpringProject/WebCommunity/Domain/Member.java
@@ -12,7 +12,7 @@ public class Member {
     @Id @Column(updatable = false, nullable = false, length = 64)
     private String id;
     @Column(nullable = false, length = 256)
-    private Long password;
+    private String password;
     @Column(nullable = false, length = 32)
     private String name;
     @Column(nullable = false, unique = true, length = 32)
@@ -22,15 +22,19 @@ public class Member {
     @Column(length = 15)
     private String contact;
     @Column(length = 128)
-    private String address;
+    private String birth;
     @Column(nullable = false, updatable = false, unique = true)
     private LocalDateTime createdTime;
     private String profileImg;
 
     @Builder // setter
-    public Member(String email, String contact, String address) {
+    public Member(String id, String password, String name, String nickName, String email, String contact, String birth) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.nickName = nickName;
         this.email = email;
         this.contact = contact;
-        this.address = address;
+        this.birth = birth;
     }
 }
