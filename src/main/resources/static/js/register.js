36f92ea337a354005a0ce7f96b5077a0ee6366c7
LazoYoung <idealproduction@naver.com>
2023-10-31 21:24:04 +0900

    회원등록 오류 피드백

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -25,15 +25,49 @@ function onInit() {
     phoneInput.addEventListener("input", validateForm);
     birthInput.addEventListener("input", validateForm);
     privacyConsent.addEventListener("input", validateForm);
-    form.addEventListener("submit", e => submitForm(e));
+    form.addEventListener("submit", e => onSubmit(e, form));
 }
 
-function submitForm(event) {
-    if (!validateForm()) {
-        event.preventDefault();
-    }
-    // todo compute response
+async function onSubmit(event, form) {
+    event.preventDefault();
 
+    if (!validateForm()) return;
+
+    let r = await fetch("/member/register", {
+        method: "POST",
+        body: new FormData(form)
+    });
+    let json = await r.json();
+
+    if (r.ok) {
+        location.href = "/";
+    } else {
+        onError(json);
+    }
+}
+
+function onError(response) {
+    let status = parseInt(response["status"]);
+
+    switch (status) {
+        case 1:  // INCOMPLETE_FORM
+            window.alert("누락된 내용이 있습니다.");
+            break;
+        case 2:  // SHORT_PASSWORD
+            updateInput(passwordInput, false);
+            window.alert("비밀번호는 최소 8글자로 구성해야 합니다.")
+            break;
+        case 3:  // DUPLICATE_EMAIL
+            updateInput(emailInput, false);
+            window.alert("본 이메일은 이미 사용중입니다.");
+            break;
+        case 4:  // DUPLICATE_NAME
+            updateInput(nicknameInput, false);
+            window.alert("본 이름은 이미 사용중입니다.");
+            break;
+        default:
+            window.alert("서버 오류!");
+    }
 }
 
 function validateForm() {
@@ -45,11 +79,11 @@ function validateForm() {
     let birth = birthInput.value;
     let consent = privacyConsent.checked;
 
-    return (validate(emailInput, email && email.includes("@"))
+    return (validate(emailInput, (email && email.includes("@")))
         && validate(passwordInput, (password1 && password1.length >= 8))
         && validate(passwordCheck, (password1 === password2))
         && validate(nicknameInput, nickname)
-        && validate(phoneInput, phone && phone.match(/^[0-9]+$/) != null)
+        && validate(phoneInput, (phone && phone.match(/^[0-9]+$/) != null))
         && validate(birthInput, birth)
         && validate(privacyConsent, consent));
 }
