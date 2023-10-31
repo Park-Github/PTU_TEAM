54f8f46d6ee439b196b4af03aa07766db9cef571
LazoYoung <idealproduction@naver.com>
2023-10-31 16:14:52 +0900

    회원 스키마 수정

--- src/main/java/SpringProject/WebCommunity/Domain/Member.java
+++ src/main/java/SpringProject/WebCommunity/Domain/Member.java
@@ -1,9 +1,8 @@
 package SpringProject.WebCommunity.Domain;
 
+import SpringProject.WebCommunity.Service.SecurityService;
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 @Entity
@@ -11,28 +10,37 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Table(name = "member")
 public class Member {
-    @Id @Column(updatable = false, nullable = false)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false, length = 256)
-    private String password;
+    @Column(nullable = false, length = SecurityService.SALT_LENGTH)
+    private String passwordSalt;
+    @Column(nullable = false, length = SecurityService.HASH_LENGTH)
+    private String passwordHash;
     @Column(nullable = false, unique = true, length = 64)
     private String nickName;
-    @Column(nullable = false, length = 64)
+    @Column(nullable = false, unique = true, length = 64)
     private String email;
     @Column(length = 15)
     private String contact;
     @Column(length = 128)
     private String birth;
-    @Column(nullable = false, updatable = false, unique = true)
+    @Temporal(TemporalType.TIMESTAMP) @Column(nullable = false)
     private LocalDateTime createdTime;
     private String profileImg;
 
-    @Builder // setter
-    public Member(String password,String nickName, String email, String contact, String birth) {
-        this.password = password;
+    @Builder
+    public Member(String passwordHash, String passwordSalt, String nickName, String email, String contact, String birth) {
+        this.passwordHash = passwordHash;
+        this.passwordSalt = passwordSalt;
         this.nickName = nickName;
         this.email = email;
         this.contact = contact;
         this.birth = birth;
     }
+
+    @PrePersist
+    public void onCreate() {
+        this.createdTime = LocalDateTime.now();
+    }
+
 }
