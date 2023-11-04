183d58d15bbb1fcaf1918d1def10ab7c35913f11
LazoYoung <idealproduction@naver.com>
2023-11-04 21:02:34 +0900

    #4 필터를 통한 URL 요청 인가

---
+++ src/main/java/SpringProject/WebCommunity/SecurityConfig.java
@@ -0,0 +1,82 @@
+package SpringProject.WebCommunity;
+
+import SpringProject.WebCommunity.Repository.MemberRepos;
+import SpringProject.WebCommunity.Service.MemberDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(c -> c
+                        // 기본 리소스 접근 허용
+                        .requestMatchers(antMatcher("/css/**")).permitAll()
+                        .requestMatchers(antMatcher("/img/**")).permitAll()
+                        .requestMatchers(antMatcher("/js/**")).permitAll()
+                        .requestMatchers(antMatcher("/error/**")).permitAll()
+
+                        // 사용자 인증 요청 허용
+                        .requestMatchers(antMatcher("/login")).permitAll()
+                        .requestMatchers(antMatcher("/register")).permitAll()
+                        .requestMatchers(antMatcher("/logout")).permitAll()
+
+                        // 공개 페이지 허용
+                        .requestMatchers(antMatcher("/")).permitAll()
+
+                        // 위에 명시되지 않은 요청은 인증된 사용자를 제외하고 불허
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutRequestMatcher(antMatcher("/logout"))
+                        .logoutSuccessUrl("/")
+                        .deleteCookies("JSESSIONID")
+                        .invalidateHttpSession(true)
+                        .permitAll()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder
+    ) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(MemberRepos memberRepos) {
+        return new MemberDetailsService(memberRepos);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
