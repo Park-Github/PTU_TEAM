0ad3833e08074e26e116e89477a36f054f93af20
Park Seung Ho <dreamhigh924@gmail.com>
2023-11-12 12:35:39 +0900

    글 작성 서비스 (장터 게시판)

--- src/main/java/SpringProject/WebCommunity/Controller/MarketArticleController.java
+++ src/main/java/SpringProject/WebCommunity/Controller/MarketArticleController.java
@@ -1,8 +1,8 @@
 package SpringProject.WebCommunity.Controller;
 
 import SpringProject.WebCommunity.Model.Domain.Member;
-import SpringProject.WebCommunity.Model.Dto.BoardArticleCreateDto;
-import SpringProject.WebCommunity.Model.Dto.BoardArticleReadDto;
+import SpringProject.WebCommunity.Model.Dto.ArticleCreateDto;
+import SpringProject.WebCommunity.Model.Dto.ArticleReadDto;
 import SpringProject.WebCommunity.Service.ArticleService;
 import SpringProject.WebCommunity.Service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -27,40 +28,55 @@ public class MarketArticleController {
     private final MemberService memberService;
 
     @GetMapping("/market/form")
-    public String newArticleForm() {return "/form/market-write";}
+    public String newArticleForm(ArticleCreateDto dto,
+                                 Model model) {
+        model.addAttribute("boardArticle", dto);
+        return "/form/market-write";
+    }
 
     // 장터 게시글 Form 데이터 Post mapping
-    @PostMapping("/market/articles/create")
-    public String createMarketArticle(BoardArticleCreateDto form,
+    @PostMapping("/market/create")
+    public String createMarketArticle(ArticleCreateDto form,
                                       HttpServletRequest request,
                                       RedirectAttributes redirectAttr) {
 
         Optional<Member> member = memberService.getMember(request);
-        log.info(member.toString());
         if (member.isPresent()) {
+            String articleUri = "";
             Long boardId = articleService.saveToCreate(form);
             redirectAttr.addFlashAttribute("success", "게시글이 등록되었습니다.");
-            log.info(boardId.toString());
-            return "redirect:/market/articles/" + boardId;
+            if(form.getCategory().equals("market-buy")){
+                articleUri = "redirect:/market/buy/articles/" + boardId;
+                log.info(articleUri);
+            }
+
+            else if (form.getCategory().equals("market-sell")) {
+                articleUri = "redirect:/market/sell/articles/" + boardId;
+            }
+
+            return articleUri;
         }
-        else return "redirect:/menu/home";
+        else
+            return "redirect:/home";
+
     }
 
     // 장터 게시판 게시글 조회
-    @GetMapping("/market/articles/{id}")
-    public String showArticle(@PathVariable Long id, Model model) {
+    @GetMapping(value = {"/market/buy/articles/{id}", "/market/sell/articles/{id}"})
+    public String showArticle(@PathVariable Long id,
+                              Model model) {
         log.info("id = " + id);
-        Optional<BoardArticleReadDto> articleReadDto = Optional.ofNullable(articleService.findById(id));
-        articleReadDto.ifPresent(i -> model.addAttribute("article", i));
+        Optional<ArticleReadDto> articleReadDto = Optional.ofNullable(articleService.findById(id));
+        articleReadDto.ifPresent(i -> model.addAttribute("boardArticle", i));
         articleReadDto.orElseThrow();
-        return "/menu/market";
+        return "/menu/article";
     }
 
     // 장터 게시판 게시글 목록 조회
     @GetMapping("/market")
     public String articleListView(Model model) {
         // 모든 Article 가져오기
-        List<BoardArticleReadDto> articleList = articleService.findAll();
+        List<ArticleReadDto> articleList = articleService.findAll();
         // Model에 등록
         model.addAttribute("articleList", articleList);
         return "/menu/market";
