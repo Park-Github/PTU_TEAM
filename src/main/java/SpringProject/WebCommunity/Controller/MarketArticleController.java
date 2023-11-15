ffd20e344a964ddb167a95f88be97a042b1853f8
Park Seung Ho <dreamhigh924@gmail.com>
2023-11-15 09:36:20 +0900

    관계 추가에 따른 컨트롤러 수정

--- src/main/java/SpringProject/WebCommunity/Controller/MarketArticleController.java
+++ src/main/java/SpringProject/WebCommunity/Controller/MarketArticleController.java
@@ -44,14 +44,15 @@ public class MarketArticleController {
         Optional<Member> member = memberService.getMember(request);
         if (member.isPresent()) {
             String articleUri = "";
-            Long boardId = articleService.saveToCreate(form);
+            Member user = member.get();
+            ArticleCreateDto dto = new ArticleCreateDto(form.getTitle(), form.getContents(), form.getCategory(), user);
+            Long boardId = articleService.saveToCreate(dto);
             redirectAttr.addFlashAttribute("success", "게시글이 등록되었습니다.");
-            if(form.getCategory().equals("market-buy")){
+            if(dto.getCategory().equals("market-buy")){
                 articleUri = "redirect:/market/buy/articles/" + boardId;
-                log.info(articleUri);
             }
 
-            else if (form.getCategory().equals("market-sell")) {
+            else if (dto.getCategory().equals("market-sell")) {
                 articleUri = "redirect:/market/sell/articles/" + boardId;
             }
 
